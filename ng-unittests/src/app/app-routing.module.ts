2249d38d128226ccb6f1814d57dbbc2c9b55bb46
Elkin Siabato <elkin.siabato@bizagi.com>
2020-07-27 13:00:56 -0500

    Basic structure for steps

--- ng-unittests/src/app/app-routing.module.ts
+++ ng-unittests/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Step01Component } from './step01/step01.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Step01Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-unittests/src/app/app.module.ts
+++ ng-unittests/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Step01Component } from './step01/step01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Step01Component
   ],
   imports: [
     BrowserModule,
